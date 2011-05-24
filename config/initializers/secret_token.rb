9c2215e54c5b0e34191bcd47b159c76276d7e651
Beyond <beyond@be.to>
2011-05-25 02:33:59 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '93847afc09eb3c74b540a3e1452b72715f66c3c831769af9a9eb4b7ae7251aea777752b66168159e3a2e58a7a42eb399d8328963d91b2fc38c52e050037fffc8'
